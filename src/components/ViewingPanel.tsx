a07f4a549f5fa67b213b742be811d1fd32da2d64
Ylidox <artiomaliev2017@gmail.com>
2024-02-08 14:36:52 +0300

    add scrollbar

---
+++ src/components/ViewingPanel.tsx
@@ -0,0 +1,8 @@
+import styles from '../styles/ViewingPanel.module.scss';
+
+export const ViewingPanel = () => {
+  return (
+    <div className={styles.container}>
+    </div>
+  )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from "../components/Sidebar"
+import { ViewingPanel } from "../components/ViewingPanel"
+
+export const Home = () => {
+  return (
+    <>
+      <Sidebar/>
+      <ViewingPanel/>
+    </>
+  )
+}
