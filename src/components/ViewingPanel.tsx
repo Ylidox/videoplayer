46e1e0db4449cac077b8c313b60331ab7a3c53ad
Ylidox <artiomaliev2017@gmail.com>
2024-02-14 12:31:22 +0300

    add grid

--- src/components/ViewingPanel.tsx
+++ src/components/ViewingPanel.tsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import { useViewingPanel } from '../hooks/useViewingPanel';
+import { CiGrid41 } from "react-icons/ci";
 import styles from '../styles/ViewingPanel.module.scss';
 import { ICamera } from './Camera';
 import { useRef } from 'react';
@@ -22,11 +23,56 @@ export const ViewingPanel = () => {
       setWindows([...windows]);
     }
   }
+
+  let grid = () => {
+    let w = 400, h = 250;
+    let count_j = window.innerWidth / w | 0;
+    let count_i = window.innerHeight / h | 0;
+    let offset_x = (window.innerWidth - w * count_j) / 2;
+    let offset_y = windows.length < count_i * count_j ? 
+      (window.innerHeight - h * count_i) / 2 : 0;
+
+    // setWindows([...windows.map(item => {item.coord.x++; return item})]);
+
+    let counter = (count_j: number) => {
+      let prev = [0, 0];
+      let coord = [0, 0];
+      let increment = () => {
+        prev = [...coord];
+        coord[0]++;
+        if(coord[0] == count_j){
+          coord[0] = 0;
+          coord[1]++;
+        }
+        return prev;
+      }
+      return increment;
+    }
+
+    let incr = counter(count_j);
+
+    for(let i = 0; i < windows.length; i++){
+      let coord = incr();
+      windows[i].coord = {
+        x: offset_x + w * coord[0],
+        y: offset_y + h * coord[1],
+      }
+      console.log('name: ', windows[i].camera.name,
+      'y: ', windows[i].coord.x,
+      'y: ', windows[i].coord.y)
+    }
+    setWindows([...windows]);
+  }
   
   let dragConst = useRef<HTMLDivElement>(null);
   return (
     <motion.div className={styles.container} onDrop={drop} onDragOver={(e) => e.preventDefault()} ref={dragConst} 
     >
+      <div className={styles.grid}
+        onClick={grid}
+      >
+        <CiGrid41/>
+      </div>
       {windows.map(item => <Window window={item} dragConst={dragConst} key={item.camera.id}/>)}
     </motion.div>
   )
